5a1390b018fa3875fb3adfd0380a02f0b87e9da7
UID <UID@192.168.92.1>
2019-09-30 15:29:29 +0800

    1

--- sp01-commons/src/main/java/com/tedu/sp01/service/OrderService.java
+++ sp01-commons/src/main/java/com/tedu/sp01/service/OrderService.java
@@ -1,6 +1,6 @@
 package com.tedu.sp01.service;
 import com.tedu.sp01.pojo.Order;
 public interface OrderService {
-Order getOrder(String orderId);
-void addOrder(Order order);
+	Order getOrder(String orderId);
+	void addOrder(Order order);
 }
